5b5d025ef633d76f7d3d6e54901cfd86b07e7b97
MUBARAK BASHIR <muburarbash280@gmail.com>
2026-09-12 14:32:47 +0100

    Private keys changed.

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // config.php - Load environment variables (MySQLi)
 $host = getenv('DB_HOST') ?: 'localhost';
-$db_name = getenv('DB_NAME') ?: 'mubeetec_portfolio_db';
-$username = getenv('DB_USER') ?: 'mubeetec_mubee';
-$password = getenv('DB_PASS') ?: 'Mubee2019@';
+$db_name = getenv('DB_NAME') ?: '***************';
+$username = getenv('DB_USER') ?: '***************';
+$password = getenv('DB_PASS') ?: '***************';
 
 $conn = mysqli_connect($host, $username, $password, $db_name);
 if (!$conn) {
